f334ccce11b8a4eb75c4cf1e21a31921ac53062b
kristiyan2022 <contact.kristiyan@gmail.com>
2026-06-26 16:11:23 +0200

    chore: add biome and re-format repo

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -7,29 +7,29 @@ import { ThemeProvider } from "@/components/theme-provider";
 const publicSans = Public_Sans({ subsets: ["latin"], variable: "--font-sans" });
 
 const fontMono = Geist_Mono({
-	subsets: ["latin"],
-	variable: "--font-mono",
+  subsets: ["latin"],
+  variable: "--font-mono",
 });
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html
-			lang="en"
-			suppressHydrationWarning
-			className={cn(
-				"antialiased",
-				fontMono.variable,
-				"font-sans",
-				publicSans.variable,
-			)}
-		>
-			<body>
-				<ThemeProvider>{children}</ThemeProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        publicSans.variable,
+      )}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  );
 }
