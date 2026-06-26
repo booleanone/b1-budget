aebc181414868ddf5d38cb922657510aa3889ae5
kristiyan2022 <contact.kristiyan@gmail.com>
2026-06-26 15:23:47 +0200

    chore: add Google-style Checkstyle and Lombok configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,19 @@ description = "B1 Budget – budgeting platform"
 
 subprojects {
 	plugins.withType<JavaPlugin> {
+		apply(plugin = "checkstyle")
+
+		configurations {
+			compileOnly {
+				extendsFrom(configurations.annotationProcessor.get())
+			}
+		}
+
 		dependencies {
+			compileOnly(rootProject.libs.lombok)
+			annotationProcessor(rootProject.libs.lombok)
+			testCompileOnly(rootProject.libs.lombok)
+			testAnnotationProcessor(rootProject.libs.lombok)
 			testRuntimeOnly(rootProject.libs.junit.platform.launcher)
 		}
 
@@ -25,6 +37,14 @@ subprojects {
 
 		tasks.withType<Test> {
 			useJUnitPlatform()
+		}
+
+		extensions.configure<CheckstyleExtension> {
+			toolVersion = rootProject.libs.versions.checkstyle.get()
+			configFile = rootProject.file("${rootDir}/.checkstyle/checkstyle.xml")
+			configProperties = mapOf(
+				"suppressionFile" to "${rootDir}/.checkstyle/checkstyle-suppressions.xml"
+			)
 		}
 	}
 }
